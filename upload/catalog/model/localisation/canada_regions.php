8dfddc846cc5f7a23f7df7c11532c85a6ae26542
Emman Calabroso <emman.calabroso@chromedia.com>
2014-07-03 19:10:08 +0800

    merging files

--- upload/catalog/model/localisation/canada_regions.php
+++ upload/catalog/model/localisation/canada_regions.php
@@ -5,7 +5,7 @@ class ModelLocalisationCanadaRegions extends Model {
         $canada_regions = $this->cache->get('canada_regions');
         
         if (!$canada_regions) {
-            $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "canada_regions WHERE 1 ORDER BY name ASC");
+            $query = $this->db->query("SELECT * FROM canada_regions WHERE 1 ORDER BY name ASC");
     
             $canada_regions = $query->rows;
         
